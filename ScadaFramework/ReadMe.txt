33e9d81807851adbb6f329a41fe98223f18d29d7
jimzhang3 <jimzhang3@163.com>
2013-08-08 16:15:56 +0800

    first time  commit!

---
+++ ScadaFramework/ReadMe.txt
@@ -0,0 +1,6 @@
+﻿2013年8月6日 22:49:17		
+主模块定义：
+SCADA		调试运行，数据采集运行，控制运行
+Protocol	协议组织
+Simulator	设备模拟(设备模拟对协议组织有帮助，十分建议Protocol继承自Simulator)
+Application	应用程序业务逻辑
---
+++ ScadaFramework/Simulator/Categories/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScadaFramework.Simulator.Categories
+{
+    enum Category
+    {
+        Printer,
+        PLC,
+        INV,
+        UPS,
+        PC
+    }
+}
